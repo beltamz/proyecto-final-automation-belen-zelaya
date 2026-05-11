612f7be9b21c943db7ef85220213ab60bba06fea
Belen Zelaya <belenzelaya21@gmail.com>
2026-05-10 22:50:29 -0300

    Login agregado

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -3,11 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
-def test_login_validation(login_in_driver):
+def test_inventory(login_in_driver):
     try:
         driver= login_in_driver
-        assert "/inventory.html" in driver.current_url, "No se redirigio al inventario"
 
+        
     except Exception as e:
-        print(f"Error en test_login: {e}")
-        raise
+        print(f"Error en test_inventory: {e}")
