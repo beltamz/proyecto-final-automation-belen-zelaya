e75a79f0fc63bca92c8725e67357c77dfb59cfbc
Belen Zelaya <belenzelaya21@gmail.com>
2026-07-08 00:30:59 -0300

    Comentarios finales

--- conftest.py
+++ conftest.py
@@ -22,39 +22,38 @@ def login_in_driver(driver):
     login_page.login(user["username"] , user["password"] )
     return driver
 
+#Fixture para reutilizar InventoryPage sin tener que loguearse en c/ test
 @pytest.fixture
 def inventory_page_obj(login_in_driver):
     return InventoryPage(login_in_driver)
 
+# Fixture para reutilizar CartPage compartiendo la sesion activa del driver
 @pytest.fixture
 def cart_page_obj(login_in_driver):
     return CartPage(login_in_driver)
 
+# Hook interno de Pytest que intercepta el resultado de cada prueba al ejecutarse
 @pytest.hookimpl(tryfirst=True, hookwrapper= True)
 def pytest_runtest_makereport(item, call):
     outcome= yield
 
     report= outcome.get_result()
 
+    # Si la prueba falla durante la fase de ejecución ("call"), toma una captura de pantalla
     if report.when == "call" and report.failed:
         driver= item.funcargs.get("driver")
 
         if driver:
+            # Crea la carpeta para guardar las capturas si no existe
             target = pathlib.Path("reports/screenshots")
             target.mkdir(parents= True, exist_ok=True)
 
+            # Define el nombre de la captura usando el nombre exacto del test que fallo
             file_name= target / f"{item.name}.png"
 
             driver.save_screenshot(str(file_name))
 
-            #if hasattr(report, "extra"):
-            #    report.extra.append({
-            #        "name": "screenshot",
-            #        "format": "image",
-            #        "content": str(file_name)
-            #    })
-
+            # Adjunta la captura de pantalla directamente dentro del reporte HTML generado
             extras= getattr(report, "extras", [])
             extras.append(pytest_html.extras.png(str(file_name)))
-
             report.extras = extras
